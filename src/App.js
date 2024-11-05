738379f48e643879136b777b6462849c58ff51ed
Saad-Ahmed-03 <saad.ahmed@devmontdigital.io>
2024-11-05 16:51:56 +0500

    testing

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ export default function App() {
   }, []);
 
   return (
-    <div className="App">
+    <div className="App test">
       {/* Add the "scrolled" class to Header when isScrolled is true */}
       <header className={`App-header ${isScrolled ? 'scrolled' : ''}`}>
         <Header
